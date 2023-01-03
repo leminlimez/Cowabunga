9b81e96101b3351826b8af3386a37e3f6c06c9d0
leminlimez <jirizarry317@gmail.com>
2023-01-03 10:35:35 -0500

    Initial Commit

---
+++ DockHider/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DockHider
+//
+//  Created by lemin on 1/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DockHider/DockHiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockHiderApp.swift
+//  DockHider
+//
+//  Created by lemin on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DockHiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
