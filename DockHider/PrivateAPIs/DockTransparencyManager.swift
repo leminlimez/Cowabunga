26a32e584d5738246569e7f2ffbf396a332134f5
leminlimez <jirizarry317@gmail.com>
2023-01-03 21:03:53 -0500

    gameing

--- DockHider/PrivateAPIs/DockTransparencyManager.swift
+++ DockHider/PrivateAPIs/DockTransparencyManager.swift
@@ -19,7 +19,21 @@ func setDockFile(fileName: String, originURL: URL) throws {
     let replacementURL = Bundle.main.url(forResource: fileName, withExtension: "materialrecipe")!
     let replacementData = try! Data(contentsOf: replacementURL)
     
+    inProgress = true
     try FSOperation.perform(.writeData(url: originURL, data: replacementData), rootHelperConf: RootConf.shared)
+}
+
+func waitUntilFinished() -> Bool {
+    var currentTime: Int = Int(Date().timeIntervalSince1970)
+    while (true) {
+        // this is probably a bad idea but I am stupid :P
+        // set a timer until the process is done or it exceeds 30 seconds
+        if inProgress == false {
+            return true
+        } else if currentTime - Int(Date().timeIntervalSince1970) >= 30 {
+            return false
+        }
+    }
 }
 
 func applyDock(isVisible: Bool) -> Bool {
@@ -42,16 +56,25 @@ func applyDock(isVisible: Bool) -> Bool {
     // apply to the docks
     do {
         // dark
+        ApplyingVariables.applyingText = "Applying to dark mode dock..."
         try setDockFile(fileName: darkFile, originURL: URL(string: darkPath)!)
+        if !waitUntilFinished() {
+            return false
+        }
         // light
+        ApplyingVariables.applyingText = "Applying to light mode dock..."
         try setDockFile(fileName: lightFile, originURL: URL(string: lightPath)!)
+        if !waitUntilFinished() {
+            return false
+        }
     } catch {
         print("Writing failed")
         return false
     }
     
     // respring
-    //respring()
+    ApplyingVariables.applyingText = "Respringing..."
+    respring()
     
     return true
 }
