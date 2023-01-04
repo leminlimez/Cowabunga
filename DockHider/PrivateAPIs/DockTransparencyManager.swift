1d0310b2831cad95fa01192a421c9e2f9fce0dfb
leminlimez <jirizarry317@gmail.com>
2023-01-03 23:54:05 -0500

    shortened timeout slightly

--- DockHider/PrivateAPIs/DockTransparencyManager.swift
+++ DockHider/PrivateAPIs/DockTransparencyManager.swift
@@ -24,7 +24,7 @@ func setDockFile(fileName: String, originURL: URL) throws {
 }
 
 func waitUntilFinished() async throws -> Void {
-    for _ in 0..<15 {
+    for _ in 0..<12 {
         if inProgress == false {
             return
         }
