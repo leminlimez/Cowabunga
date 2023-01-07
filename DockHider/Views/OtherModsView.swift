8964452de49ab54bbfed69ba2deecd60d18675af
leminlimez <jirizarry317@gmail.com>
2023-01-07 10:35:33 -0500

    set product version

--- DockHider/Views/OtherModsView.swift
+++ DockHider/Views/OtherModsView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct OtherModsView: View {
+    @State private var CurrentVersion: String = getSystemVersion()
+    
     var body: some View {
         VStack {
             // title
@@ -18,6 +20,46 @@ struct OtherModsView: View {
             Text("Miscelaneous Modifications")
                 .font(.title2)
                 .padding(.bottom, 10)
+            HStack {
+                Image(systemName: "gear.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24, height: 24)
+                    .foregroundColor(.blue)
+                
+                Text("Product Version")
+                    .minimumScaleFactor(0.5)
+                    .padding(.trailing, 50)
+                
+                Button(CurrentVersion, action: {
+                    let defaults = UserDefaults.standard
+                    // create and configure alert controller
+                    let alert = UIAlertController(title: "Input Product Version", message: "No respring required to apply.", preferredStyle: .alert)
+                    // bring up the text prompt
+                    alert.addTextField { (textField) in
+                        textField.placeholder = "Version"
+                        textField.text = defaults.string(forKey: "ProductVersion") ?? CurrentVersion
+                    }
+                    
+                    // buttons
+                    alert.addAction(UIAlertAction(title: "Apply", style: .default) { (action) in
+                        // set the version
+                        let newVersion: String = alert.textFields?[0].text! ?? CurrentVersion
+                        setProductVersion(newVersion: newVersion) { succeeded in
+                            if succeeded {
+                                CurrentVersion = newVersion
+                                // set the default
+                                defaults.set(newVersion, forKey: "ProductVersion")
+                            }
+                        }
+                    })
+                    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+                        // cancel the process
+                    })
+                    UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
+                })
+                    .padding(.leading, 10)
+            }
         }
     }
 }
