73858688c7f6c575d6ca21fe6f7ebcc2b75304e2
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-01-10 11:13:38 -0500

    changing model name

--- DockHider/Views/OtherModsView.swift
+++ DockHider/Views/OtherModsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct OtherModsView: View {
     @State private var CurrentVersion: String = getSystemVersion()
+    @State private var CurrentModel: String = getModel()
     
     var body: some View {
         VStack {
@@ -46,11 +47,54 @@ struct OtherModsView: View {
                         // set the version
                         let newVersion: String = alert.textFields?[0].text! ?? CurrentVersion
                         if newVersion != "" {
-                            setProductVersion(newVersion: newVersion) { succeeded in
+                            setPlistValue(plistPath: "/System/Library/CoreServices/SystemVersion.plist", backupName: "CoreServices/SystemVersion.plist", key: "ProductVersion", newValue: newVersion) { succeeded in
                                 if succeeded {
                                     CurrentVersion = newVersion
                                     // set the default
                                     defaults.set(newVersion, forKey: "ProductVersion")
+                                }
+                            }
+                        }
+                    })
+                    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+                        // cancel the process
+                    })
+                    UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
+                })
+                    .padding(.leading, 10)
+            }
+            
+            HStack {
+                Image(systemName: "iphone")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24, height: 24)
+                    .foregroundColor(.blue)
+                
+                Text("Model Name")
+                    .minimumScaleFactor(0.5)
+                    .padding(.trailing, 50)
+                
+                Button(CurrentModel, action: {
+                    let defaults = UserDefaults.standard
+                    // create and configure alert controller
+                    let alert = UIAlertController(title: "Input Model Name", message: "No respring required to apply.", preferredStyle: .alert)
+                    // bring up the text prompt
+                    alert.addTextField { (textField) in
+                        textField.placeholder = "Model"
+                        textField.text = defaults.string(forKey: "ModelName") ?? CurrentModel
+                    }
+                    
+                    // buttons
+                    alert.addAction(UIAlertAction(title: "Apply", style: .default) { (action) in
+                        // set the version
+                        let newModel: String = alert.textFields?[0].text! ?? CurrentModel
+                        if newModel != "" {
+                            setPlistValue(plistPath: "/var/containers/Shared/SystemGroup/systemgroup.com.apple.mobilegestaltcache/Library/Caches/com.apple.MobileGestalt.plist", backupName: "com.apple.MobileGestalt.plist", key: "ArtworkDeviceProductDescription", newValue: newModel) { succeeded in
+                                if succeeded {
+                                    CurrentModel = newModel
+                                    // set the default
+                                    defaults.set(newModel, forKey: "ModelName")
                                 }
                             }
                         }
