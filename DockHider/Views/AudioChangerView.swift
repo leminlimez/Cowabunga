6abec6f28b10d98c9206db6cbd96543313fe9a2d
leminlimez <jirizarry317@gmail.com>
2023-01-10 18:12:05 -0500

    audio changer view

--- DockHider/Views/AudioChangerView.swift
+++ DockHider/Views/AudioChangerView.swift
@@ -10,21 +10,70 @@ import SwiftUI
 struct AudioChangerView: View {
     var SoundIdentifier: String
     
+    // included audio files
+    struct IncludedAudioName: Identifiable {
+        var id = UUID()
+        var attachment: String
+        var audioName: String
+        var fileName: String
+        var checked: Bool = false
+    }
+    
+    // list of included audio files
+    @State var audioFiles: [IncludedAudioName] = [
+        .init(attachment: "Charging", audioName: "Default", fileName: "Default_Charging"),
+        .init(attachment: "Charging", audioName: "AirPower", fileName: "AirPower_Charging"),
+    ]
+    
+    // applied sound
+    @State private var appliedSound: String = "Default"
+    
     var body: some View {
-        var AppliedSound = UserDefaults.standard.string(forKey: SoundIdentifier+"_Applied") ?? "Default"
         VStack {
             NavigationView {
                 List {
                     Section {
-                        HStack {
-                            Button("Default", action: {
-                                
-                            })
-                                .padding(.horizontal, 8)
+                        ForEach($audioFiles) { audio in
+                            if audio.attachment.wrappedValue == SoundIdentifier {
+                                // create button
+                                // idk what I am doing with this but okay
+                                HStack {
+                                    Image(systemName: "checkmark")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 15, height: 15)
+                                        .foregroundColor(.blue)
+                                        .opacity(audio.checked.wrappedValue ? 1: 0)
+                                    
+                                    Button(audio.audioName.wrappedValue, action: {
+                                        if appliedSound != audio.audioName.wrappedValue {
+                                            for (i, file) in audioFiles.enumerated() {
+                                                if file.audioName == appliedSound {
+                                                    audioFiles[i].checked = false
+                                                } else if file.audioName == audio.audioName.wrappedValue {
+                                                    audioFiles[i].checked = true
+                                                }
+                                            }
+                                            appliedSound = audio.audioName.wrappedValue
+                                            // save to defaults
+                                            UserDefaults.standard.set(appliedSound, forKey: SoundIdentifier+"_Applied")
+                                        }
+                                    })
+                                    .padding(.horizontal, 8)
+                                }
+                            }
                         }
                     } header: {
-                        Text("Choose a new sound")
+                        Text("Included")
                     }
+                }
+            }
+        }
+        .onAppear {
+            appliedSound = UserDefaults.standard.string(forKey: SoundIdentifier+"_Applied") ?? "Default"
+            for (i, file) in audioFiles.enumerated() {
+                if file.audioName == appliedSound {
+                    audioFiles[i].checked = true
                 }
             }
         }
