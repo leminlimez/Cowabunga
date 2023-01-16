20ec6b4f19ad60dfac462c3d04806345c0d7a17f
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-01-16 17:09:24 -0500

    new grid ui layout

--- DockHider/Views/ContentView.swift
+++ DockHider/Views/ContentView.swift
@@ -10,14 +10,8 @@ import SwiftUI
 var inProgress = false
 
 struct SpringBoardView: View {
-    struct GeneralOption: Identifiable {
-        var value: Bool
-        var id = UUID()
-        var key: String
-        var title: String
-        var imageName: String
-        var fileType: OverwritingFileTypes
-    }
+    // lazyvgrid
+    private var gridItemLayout = [GridItem(.adaptive(minimum: 130))]
     
     // list of options
     @State var tweakOptions: [GeneralOption] = [
@@ -34,19 +28,27 @@ struct SpringBoardView: View {
             NavigationView {
                 List {
                     Section {
-                        ForEach($tweakOptions) { option in
-                            HStack {
-                                Image(systemName: option.imageName.wrappedValue)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 24, height: 24)
-                                    .foregroundColor(.blue)
-                                
-                                Toggle(isOn: option.value) {
-                                    Text(option.title.wrappedValue)
-                                        .minimumScaleFactor(0.5)
+                        LazyVGrid(columns: gridItemLayout) {
+                            ForEach($tweakOptions) { option in
+                                Button(action: {
+                                    option.wrappedValue.value.toggle()
+                                }) {
+                                    VStack {
+                                        Image(systemName: option.wrappedValue.value ? "checkmark.circle" : option.imageName.wrappedValue)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 24, height: 24)
+                                            .foregroundColor(.blue)
+                                            .opacity(option.wrappedValue.value ? 1 : 0.5)
+                                        
+                                        Text(option.title.wrappedValue)
+                                            .foregroundColor(.init(uiColor14: .label))
+                                    }
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                    .padding(.vertical)
+                                    .background(Color(uiColor14: option.wrappedValue.value ? .init(hue: 0.6, saturation: 1, brightness: 1, alpha: 0.15) : .secondarySystemBackground))
+                                    .cornerRadius(10)
                                 }
-                                .padding(.leading, 10)
                             }
                         }
                     } header: {
@@ -60,7 +62,7 @@ struct SpringBoardView: View {
                             if #available(iOS 15.0, *) {
                                 Text("Apply")
                                     .frame(maxWidth: .infinity)
-                                    .padding(6)
+                                    .padding(8)
                                     .buttonStyle(.bordered)
                                     .tint(.blue)
                                     .cornerRadius(8)
@@ -68,7 +70,7 @@ struct SpringBoardView: View {
                                 // Fallback on earlier versions
                                 Text("Apply")
                                     .frame(maxWidth: .infinity)
-                                    .padding(6)
+                                    .padding(8)
                                     .cornerRadius(8)
                             }
                         }
@@ -79,7 +81,7 @@ struct SpringBoardView: View {
                             if #available(iOS 15.0, *) {
                                 Text("Respring")
                                     .frame(maxWidth: .infinity)
-                                    .padding(6)
+                                    .padding(8)
                                     .buttonStyle(.bordered)
                                     .tint(.red)
                                     .cornerRadius(8)
@@ -87,7 +89,7 @@ struct SpringBoardView: View {
                                 // Fallback on earlier versions
                                 Text("Respring")
                                     .frame(maxWidth: .infinity)
-                                    .padding(6)
+                                    .padding(8)
                                     .cornerRadius(8)
                             }
                         }
@@ -127,6 +129,15 @@ struct SpringBoardView: View {
                 }
             }
         }
+    }
+    
+    struct GeneralOption: Identifiable {
+        var value: Bool
+        var id = UUID()
+        var key: String
+        var title: String
+        var imageName: String
+        var fileType: OverwritingFileTypes
     }
 }
 
