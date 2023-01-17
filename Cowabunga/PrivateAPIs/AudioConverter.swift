4f98020e76c8a47312ec340b49d8b2a41c0acd5c
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-01-17 17:28:28 -0500

    fix audio converter

--- Cowabunga/PrivateAPIs/AudioConverter.swift
+++ Cowabunga/PrivateAPIs/AudioConverter.swift
@@ -13,34 +13,28 @@ import UIKit
 func customaudio(fileURL: URL) -> String? {
     // Converting options
     var options = FormatConverter.Options()
-    options.format = AudioFileFormat.m4a
+    options.format = AudioFileFormat.caf
     options.sampleRate = 11025
     options.bitDepth = 16
     // Temp Path
     let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
-    let newURL = URL(fileURLWithPath: "\(temporaryDirectoryURL)/audio.m4a")
+    let newURL = URL(fileURLWithPath: "/var/containers/Bundle/Application/3B284421-75B1-4675-BFC5-A836A7EEE337/")
+    let newURLWithFile = URL(fileURLWithPath: "\(newURL)")
     // Delete if old file in temp dir
-    if FileManager.default.fileExists(atPath: newURL.path) {
-        do {
-            try FileManager.default.removeItem(at: newURL)
-        }
-        catch {
-            print(error)
-        }
-    }
     // CONVERT !
     let converter = FormatConverter(inputURL: fileURL, outputURL: newURL, options: options)
     converter.start { error in
         print("CONVERTER")
         print(error)
+        UIApplication.shared.alert(body: "Cannot convert you're file \(error)")
         print("CONVERTER END")
     }
     // Check file size
     do {
-        let attributes = try FileManager.default.attributesOfItem(atPath: newURL.path)
+        let attributes = try FileManager.default.attributesOfItem(atPath: newURLWithFile.path)
         let fileSize = attributes[.size] as! Int64
         if fileSize > 15000 {
-            UIApplication.shared.alert(body: "You're file is too big. Please crop or compress it to under 15 kB.")
+            UIApplication.shared.alert(body: "Your file is too big. Please crop or compress it to under 15 kB.")
             return nil
         }
     } catch {
