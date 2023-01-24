ff81448949da2b24a55566328d2aa9740ea9a4b2
leminlimez <jirizarry317@gmail.com>
2023-01-24 10:08:14 -0500

    improved file size checking

--- Cowabunga/PrivateAPIs/AudioConverter.swift
+++ Cowabunga/PrivateAPIs/AudioConverter.swift
@@ -12,57 +12,67 @@ import UIKit
 
 func customaudio(fileURL: URL, completion: @escaping (Data?) -> Void) {
     // config
-    let fileLimit: Int = 50 // in kB
+    let fileLimit: Int = 75 // in kB
     
     DispatchQueue.global(qos: .userInteractive).async {
-        // Temp Path
-        let newURL = AudioFiles.getAudioDirectory()!.appendingPathComponent("USR_" + fileURL.deletingPathExtension().lastPathComponent + ".m4a")
-        // Delete if old file in temp dir
-        // CONVERT !
-        let asset = AVURLAsset(url: fileURL)
-        let exporter = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetAppleM4A)
-        exporter?.outputFileType = AVFileType.m4a
-        exporter?.outputURL = newURL
-        exporter?.exportAsynchronously(completionHandler: {
-            if let e = exporter {
-                switch e.status {
-                case .failed:
-                    print("failed to convert audio")
-                    UIApplication.shared.alert(body: "Failed to convert audio")
-                    DispatchQueue.main.async {
-                        completion(nil)
-                    }
-                case .cancelled:
-                    print("audio conversion cancelled")
-                    DispatchQueue.main.async {
-                        completion(nil)
-                    }
-                default:
-                    // Check file size
-                    do {
-                        let attributes = try FileManager.default.attributesOfItem(atPath: fileURL.path)
-                        let fileSize = attributes[.size] as! Int64
-                        if fileSize > fileLimit*1000 {
-                            print(fileSize)
-                            UIApplication.shared.alert(body: "Your file is too big. Please crop or compress it to under "+String(fileLimit)+" kB.")
+        do {
+            if try Data(contentsOf: fileURL).count < fileLimit*1000 {
+                // Temp Path
+                let newURL = AudioFiles.getAudioDirectory()!.appendingPathComponent("USR_" + fileURL.deletingPathExtension().lastPathComponent + ".m4a")
+                // Delete if old file in temp dir
+                // CONVERT !
+                let asset = AVURLAsset(url: fileURL)
+                let exporter = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetAppleM4A)
+                exporter?.outputFileType = AVFileType.m4a
+                exporter?.outputURL = newURL
+                exporter?.exportAsynchronously(completionHandler: {
+                    if let e = exporter {
+                        switch e.status {
+                        case .failed:
+                            print("failed to convert audio")
+                            UIApplication.shared.alert(body: "Failed to convert audio")
                             DispatchQueue.main.async {
                                 completion(nil)
                             }
-                        } else {
-                            let fileData = try Data.init(contentsOf: fileURL)
+                        case .cancelled:
+                            print("audio conversion cancelled")
                             DispatchQueue.main.async {
-                                completion(fileData)
+                                completion(nil)
+                            }
+                        default:
+                            // Check file size
+                            do {
+                                let attributes = try FileManager.default.attributesOfItem(atPath: fileURL.path)
+                                let fileSize = attributes[.size] as! Int64
+                                if fileSize > fileLimit*1000 {
+                                    print(fileSize)
+                                    UIApplication.shared.alert(body: "Your file is too big. Please crop or compress it to under "+String(fileLimit)+" kB.")
+                                    DispatchQueue.main.async {
+                                        completion(nil)
+                                    }
+                                } else {
+                                    let fileData = try Data.init(contentsOf: fileURL)
+                                    DispatchQueue.main.async {
+                                        completion(fileData)
+                                    }
+                                }
+                            } catch {
+                                print("Error: Unable to check file size.")
+                                UIApplication.shared.alert(body: "Unable to verify file size.")
+                                DispatchQueue.main.async {
+                                    completion(nil)
+                                }
                             }
                         }
-                    } catch {
-                        print("Error: Unable to check file size.")
-                        UIApplication.shared.alert(body: "Unable to verify file size.")
-                        DispatchQueue.main.async {
-                            completion(nil)
-                        }
                     }
-                }
+                })
             }
-        })
+        } catch {
+            print("File size too big")
+            UIApplication.shared.alert(body: "Your file is too big. Please crop or compress it to under "+String(fileLimit)+" kB.")
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+        }
     }
 }
