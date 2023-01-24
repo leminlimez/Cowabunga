a9f6c21f6bee43dc0f1e6af6d2ec50f0e4f4e8a5
leminlimez <jirizarry317@gmail.com>
2023-01-24 14:19:07 -0500

    removed region restrictions options

--- Cowabunga/Views/NavigatorViews/SpringBoardView.swift
+++ Cowabunga/Views/NavigatorViews/SpringBoardView.swift
@@ -18,7 +18,7 @@ struct SpringBoardView: View {
         .init(value: getDefaultBool(forKey: "DockHidden"), key: "DockHidden", title: "Hide Dock", imageName: "dock.rectangle", fileType: OverwritingFileTypes.springboard),
         .init(value: getDefaultBool(forKey: "HomeBarHidden"), key: "HomeBarHidden", title: "Hide Home Bar", imageName: "iphone", fileType: OverwritingFileTypes.springboard),
         .init(value: getDefaultBool(forKey: "FolderBGHidden"), key: "FolderBGHidden", title: "Disable Folder Background", imageName: "folder", fileType: OverwritingFileTypes.springboard),
-        .init(value: getDefaultBool(forKey: "RegionRestrictionsRemoved"), key: "RegionRestrictionsRemoved", title: "Remove Region Restrictions", imageName: "globe", fileType: OverwritingFileTypes.region),
+        .init(value: getDefaultBool(forKey: "FolderBlurDisabled"), key: "FolderBlurDisabled", title: "Disable Folder Blur", imageName: "folder.circle", fileType: OverwritingFileTypes.springboard),
         .init(value: getDefaultBool(forKey: "SwitcherBlurDisabled"), key: "SwitcherBlurDisabled", title: "Disable App Switcher Blur", imageName: "apps.iphone", fileType: OverwritingFileTypes.springboard),
         .init(value: getDefaultBool(forKey: "ShortcutBannerDisabled"), key: "ShortcutBannerDisabled", title: " Disable Shortcut Banner ", imageName: "pencil.slash", fileType: OverwritingFileTypes.plist),
     ]
