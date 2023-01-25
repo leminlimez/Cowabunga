9ef9fdfec45045efbdd88cdbf3458facdf4080fd
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-01-25 09:15:34 -0500

    update version

--- Cowabunga/Views/NavigatorViews/HomeView.swift
+++ Cowabunga/Views/NavigatorViews/HomeView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var versionBuild: String = ""
+    @State private var versionBuild: String = " (beta 1)"
     // list of options
     @State var tweakOptions: [GeneralOption] = [
         .init(key: "DockHidden", fileType: OverwritingFileTypes.springboard),
