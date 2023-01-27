87fa8cd30e5423d17b8be69d6a73b1934134c76a
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-01-27 10:05:05 -0500

    start of custom locks

---
+++ Cowabunga/Views/UniqueToolsViews/LockView.swift
@@ -0,0 +1,28 @@
+//
+//  LockView.swift
+//  Cowabunga
+//
+//  Created by lemin on 1/27/23.
+//
+// Credit to Haxi0 for original TrollLock
+
+import Foundation
+import SwiftUI
+
+struct LockView: View {
+    var body: some View {
+        VStack {
+            Button(action: {
+                
+            }) {
+                Text("Test")
+            }
+        }
+    }
+}
+
+struct LockView_Previews: PreviewProvider {
+    static var previews: some View {
+        LockView()
+    }
+}
