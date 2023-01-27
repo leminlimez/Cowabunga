dc2a1ba93c7ff036b34bfb804796a410afbe387d
leminlimez <jirizarry317@gmail.com>
2023-01-27 09:30:31 -0500

    improve device model getter

--- Cowabunga/CowabungaApp.swift
+++ Cowabunga/CowabungaApp.swift
@@ -124,3 +124,17 @@ extension Bundle {
         return infoDictionary?["CFBundleVersion"] as? String
     }
 }
+
+extension UIDevice {
+    var machineName: String {
+        if let simulatorModelIdentifier = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] {
+            return simulatorModelIdentifier
+        }
+
+        var sysinfo = utsname()
+        uname(&sysinfo) // ignore return value
+        let deviceModel = String(bytes: Data(bytes: &sysinfo.machine, count: Int(_SYS_NAMELEN)), encoding: .ascii)?.trimmingCharacters(in: .controlCharacters)
+
+        return deviceModel ?? ""
+    }
+}
