6109e8102344743b5857e65aea90aee321abde41
leminlimez <jirizarry317@gmail.com>
2023-01-27 13:46:25 -0500

    headers

---
+++ Cowabunga/grant_full_disk_access.h
@@ -0,0 +1,14 @@
+//
+//  grant_full_disk_access.h
+//  Cowabunga
+//
+//  Created by lemin on 1/27/23.
+//
+
+#ifndef grant_full_disk_access_h
+#define grant_full_disk_access_h
+
+void grant_full_disk_access(void);
+
+
+#endif /* grant_full_disk_access_h */
