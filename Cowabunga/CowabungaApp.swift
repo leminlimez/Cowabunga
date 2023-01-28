89ed6c95bd597afd851b9638621d6fe67c31f89b
leminlimez <jirizarry317@gmail.com>
2023-01-28 16:55:15 -0500

    dont run mdc if ts

--- Cowabunga/CowabungaApp.swift
+++ Cowabunga/CowabungaApp.swift
@@ -16,10 +16,24 @@ struct CowabungaApp: App {
         WindowGroup {
             RootView()
                 .onAppear {
-                    // grant r/w access
-                    if #available(iOS 15, *) {
-                        grant_full_disk_access() { error in
-                            
+                    if #available(iOS 16.2, *) {
+                        UIApplication.shared.alert(title: "Not Supported", body: "This version of iOS is not supported.")
+                    } else {
+                        do {
+                            // TrollStore method
+                            try FileManager.default.contentsOfDirectory(at: URL(fileURLWithPath: "/var/mobile"), includingPropertiesForKeys: nil)
+                        } catch {
+                            // MDC method
+                            // grant r/w access
+                            if #available(iOS 15, *) {
+                                grant_full_disk_access() { error in
+                                    if (error != nil) {
+                                        UIApplication.shared.alert(title: "Access Error", body: "Error: \(String(describing: error?.localizedDescription))\nPlease close the app and retry.")
+                                    }
+                                }
+                            } else {
+                                UIApplication.shared.alert(title: "MDC Not Supported", body: "Please install via TrollStore")
+                            }
                         }
                     }
                     // credit: TrollTools
