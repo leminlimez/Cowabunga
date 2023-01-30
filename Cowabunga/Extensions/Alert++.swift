5f0ad32bf654412cb539d80cad1b78e7a679bf5d
Matthew Anisovich <anisovich.matthew@gmail.com>
2023-01-30 13:32:54 +0000

    login, refactor, Explore tab

---
+++ Cowabunga/Extensions/Alert++.swift
@@ -0,0 +1,55 @@
+//
+//  Alert++.swift
+//  Cowabunga
+//
+//  Created by sourcelocation on 30/01/2023.
+//
+
+import UIKit
+
+// credit: sourcelocation & TrollTools
+var currentUIAlertController: UIAlertController?
+
+extension UIApplication {
+    func dismissAlert(animated: Bool) {
+        DispatchQueue.main.async {
+            currentUIAlertController?.dismiss(animated: animated)
+        }
+    }
+    func alert(title: String = "Error", body: String, animated: Bool = true, withButton: Bool = true) {
+        DispatchQueue.main.async {
+            currentUIAlertController = UIAlertController(title: title, message: body, preferredStyle: .alert)
+            if withButton { currentUIAlertController?.addAction(.init(title: "OK", style: .cancel)) }
+            self.present(alert: currentUIAlertController!)
+        }
+    }
+    func confirmAlert(title: String = "Error", body: String, onOK: @escaping () -> (), noCancel: Bool) {
+        DispatchQueue.main.async {
+            currentUIAlertController = UIAlertController(title: title, message: body, preferredStyle: .alert)
+            if !noCancel {
+                currentUIAlertController?.addAction(.init(title: "Cancel", style: .cancel))
+            }
+            currentUIAlertController?.addAction(.init(title: "Ok", style: noCancel ? .cancel : .default, handler: { _ in
+                onOK()
+            }))
+            self.present(alert: currentUIAlertController!)
+        }
+    }
+    func change(title: String = "Error", body: String) {
+        DispatchQueue.main.async {
+            currentUIAlertController?.title = title
+            currentUIAlertController?.message = body
+        }
+    }
+    
+    func present(alert: UIAlertController) {
+        if var topController = self.windows[0].rootViewController {
+            while let presentedViewController = topController.presentedViewController {
+                topController = presentedViewController
+            }
+            
+            topController.present(alert, animated: true)
+            // topController should now be your topmost view controller
+        }
+    }
+}
