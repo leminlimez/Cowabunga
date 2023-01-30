0fdf7a58cdaf643132aaa5d6a8f4d7e78fbeacc8
Matthew Anisovich <anisovich.matthew@gmail.com>
2023-01-30 19:49:41 +0000

    this one is actually the biggest bird

---
+++ Cowabunga/Extensions/CornerRadius++.swift
@@ -0,0 +1,25 @@
+//
+//  CornerRadius++.swift
+//  Cowabunga
+//
+//  Created by sourcelocation on 30/01/2023.
+//
+
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
--- Cowabunga/Views/NavigatorViews/Explore/ExploreView.swift
+++ Cowabunga/Views/NavigatorViews/Explore/ExploreView.swift
@@ -20,25 +20,68 @@ struct ExploreView: View {
             if themes.isEmpty {
                 ProgressView()
             } else {
-                LazyVGrid(columns: gridItemLayout) {
-                    ForEach(themes) { theme in
-                        Button {
-                            print("Downloading from \(theme.url.absoluteString)")
-                        } label: {
-                            VStack {
-                                Text(theme.name)
-                                AsyncImage(url: theme.preview) { image in
-                                    image
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                } placeholder: {
-                                    Color.gray
+                ZStack {
+                    Color(uiColor14: .secondarySystemBackground).edgesIgnoringSafeArea(.all)
+                    ScrollView {
+                        LazyVGrid(columns: gridItemLayout) {
+                            ForEach(themes) { theme in
+                                Button {
+                                    print("Downloading from \(theme.url.absoluteString)")
+                                } label: {
+                                    VStack(spacing: 0) {
+                                        AsyncImage(url: theme.preview) { image in
+                                            image
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fill)
+                                                .cornerRadius(10, corners: .topLeft)
+                                                .cornerRadius(10, corners: .topRight)
+                                        } placeholder: {
+                                            Color.gray
+                                        }
+                                        HStack {
+                                            VStack(spacing: 4) {
+                                                HStack {
+                                                    Text(theme.name)
+                                                        .foregroundColor(.black)
+                                                        .minimumScaleFactor(0.5)
+                                                    Spacer()
+                                                }
+                                                HStack {
+                                                    Text(theme.contact.values.first ?? "Unknown author")
+                                                        .foregroundColor(.secondary)
+                                                        .font(.caption)
+                                                        .minimumScaleFactor(0.5)
+                                                    Spacer()
+                                                }
+                                            }
+                                            .lineLimit(1)
+                                            Spacer()
+                                            Image(systemName: "arrow.down.circle")
+                                                .foregroundColor(.blue)
+                                        }
+                                        .padding(.horizontal, 12)
+                                        .padding(.vertical, 8)
+                                        .frame(height: 58)
+                                    }
                                 }
+                                .background(Color.white)
+                                .cornerRadius(10)
+                                .padding(4)
                             }
                         }
+                        .padding(.horizontal)
                     }
+                    .navigationTitle("Themes")
                 }
-                .padding(.horizontal)
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    UIApplication.shared.alert(title: "Submit themes", body: "")
+                } label: {
+                    Image("paperplane")
+                }
             }
         }
         .onAppear {
@@ -60,5 +103,6 @@ struct ExploreView: View {
 struct ExploreView_Previews: PreviewProvider {
     static var previews: some View {
         ExploreView()
+            .environmentObject(CowabungaAPI())
     }
 }
