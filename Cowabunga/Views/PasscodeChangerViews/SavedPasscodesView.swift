831078a8c781607b3d13a5250324a463d0925fee
leminlimez <jirizarry317@gmail.com>
2023-01-31 11:53:36 -0500

    passcode importing from saved

--- Cowabunga/Views/PasscodeChangerViews/SavedPasscodesView.swift
+++ Cowabunga/Views/PasscodeChangerViews/SavedPasscodesView.swift
@@ -11,6 +11,18 @@ struct SavedPasscodesView: View {
     @State private var savedPasscodesDir = PasscodeKeyFaceManager.getPasscodesDirectory()
     @State private var numOfSaved = 0
     
+    // passcode file
+    struct PasscodeFile: Identifiable {
+        var id = UUID()
+        var passcodeName: String
+        var passcodeImage: UIImage
+        var passcodeFile: URL
+    }
+    
+    // list of passcode files
+    @State var savedPasscodesList: [PasscodeFile] = [
+    ]
+    
     var body: some View {
         VStack {
             if numOfSaved == 0 {
@@ -18,17 +30,28 @@ struct SavedPasscodesView: View {
                     .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
             } else {
                 List {
-                    HStack {
-                        Image(systemName: "phone")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(maxWidth: 40, maxHeight: 40)
-                            .cornerRadius(10, corners: .topLeft)
-                            .cornerRadius(10, corners: .topRight)
-                        
-                        Text("Passcode Name Here")
-                            .minimumScaleFactor(0.5)
-                            .font(.system(size: 24, weight: .bold))
+                    ForEach($savedPasscodesList) { passcode in
+                        HStack {
+                            Image(uiImage: passcode.passcodeImage.wrappedValue)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(maxWidth: 40, maxHeight: 40)
+                                .cornerRadius(10, corners: .topLeft)
+                                .cornerRadius(10, corners: .topRight)
+                            
+                            Button(action: {
+                                // apply passcode file when tapped
+                                do {
+                                    try PasscodeKeyFaceManager.setFacesFromTheme(passcode.passcodeFile.wrappedValue, keySize: CGFloat(PasscodeKeyFaceManager.getDefaultFaceSize()), customX: 150, customY: 150)
+                                } catch {
+                                    print("There was an error applying passcode keys: \(error.localizedDescription)")
+                                }
+                            }) {
+                                Text(passcode.passcodeName.wrappedValue)
+                                    .minimumScaleFactor(0.5)
+                                    .font(.system(size: 24, weight: .bold))
+                            }
+                        }
                     }
                 }
             }
@@ -37,6 +60,26 @@ struct SavedPasscodesView: View {
             do {
                 if savedPasscodesDir != nil {
                     numOfSaved = try FileManager.default.contentsOfDirectory(at: savedPasscodesDir!, includingPropertiesForKeys: nil).count
+                    
+                    if numOfSaved > 0 {
+                        for passcode in try FileManager.default.contentsOfDirectory(at: savedPasscodesDir!, includingPropertiesForKeys: nil) {
+                            let passcodeURL: URL? = passcode.appendingPathComponent("theme.passthm")
+                            let passcodeName: String = passcode.lastPathComponent
+                            let passcodeImage: URL? = passcode.appendingPathComponent("preview.png")
+                            if passcodeURL != nil && passcodeImage != nil {
+                                do {
+                                    let imgData = try Data(contentsOf: passcodeImage!)
+                                    let uiImg = UIImage(data: imgData)
+                                    if uiImg != nil {
+                                        savedPasscodesList.append(PasscodeFile.init(passcodeName: passcodeName, passcodeImage: uiImg!, passcodeFile: passcodeURL!))
+                                    }
+                                } catch {
+                                    print("Error getting image data: \(error.localizedDescription)")
+                                    continue
+                                }
+                            }
+                        }
+                    }
                 }
             } catch {
                 print("Error getting contents of directory")
