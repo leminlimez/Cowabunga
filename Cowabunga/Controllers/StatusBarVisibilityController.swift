2a109a59549f7cd833c2dd03b2b4e54f36414f49
Matthew Anisovich <anisovich.matthew@gmail.com>
2023-01-31 21:54:33 +0000

    add files from itechexpert

---
+++ Cowabunga/Controllers/StatusBarVisibilityController.swift
@@ -0,0 +1,28 @@
+//
+//  HiderFuncs.swift
+//  VanishBar
+//
+//  Created by Анохин Юрий on 30.01.2023.
+//
+
+import Foundation
+
+class StatusBarVisibilityController {
+    static var shared = StatusBarVisibilityController()
+    
+    let fm = FileManager.default
+    
+    func changeStatusBarVisibility(hidden: Bool, versionOS: String) throws {
+        let overrideFile = Bundle.main.url(forResource: "statusBarOverrides\(versionOS)", withExtension: nil)
+        
+        if hidden {
+            try fm.createFile(atPath: "/var/mobile/Library/SpringBoard/statusBarOverrides", contents: Data(contentsOf: overrideFile!))
+        } else {
+            try fm.removeItem(atPath: "/var/mobile/Library/SpringBoard/statusBarOverrides")
+        }
+    }
+    
+    func isApplied() -> Bool {
+        return fm.fileExists(atPath: "/var/mobile/Library/SpringBoard/statusBarOverrides")
+    }
+}
