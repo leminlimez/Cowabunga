cb61a376c25e1ebf9b458f94625ecd454182fe8a
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-02-01 10:08:39 -0500

    create color manager file

---
+++ Cowabunga/Controllers/Tools/SpringboardColorManager.swift
@@ -0,0 +1,19 @@
+//
+//  SpringboardColorManager.swift
+//  Cowabunga
+//
+//  Created by lemin on 2/1/23.
+//
+
+import SwiftUI
+
+class SpringboardColorManager {
+    enum SpringboardType {
+        case dock
+        case folder
+    }
+    
+    static func applyColor(for: SpringboardType, color: UIColor) {
+        
+    }
+}
