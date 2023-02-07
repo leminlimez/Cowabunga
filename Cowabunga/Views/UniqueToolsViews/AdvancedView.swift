11eda8b5176e1108d6bcc3a69c5809b727d7b3eb
leminlimez <jirizarry317@gmail.com>
2023-02-07 14:14:28 -0500

    advanced types

---
+++ Cowabunga/Views/UniqueToolsViews/AdvancedView.swift
@@ -0,0 +1,22 @@
+//
+//  AdvancedView.swift
+//  Cowabunga
+//
+//  Created by lemin on 2/7/23.
+//
+
+import SwiftUI
+
+struct AdvancedView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct AdvancedView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdvancedView()
+    }
+}
