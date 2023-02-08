a2a8bc3b10871dc33dba00ad8f292b9e0d44e39c
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-02-07 20:32:34 -0500

    start of custom operations

--- Cowabunga/Controllers/AdvancedTools/AdvancedManager.swift
+++ Cowabunga/Controllers/AdvancedTools/AdvancedManager.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 class AdvancedManager {
-    
+    func getSavedOperationsDirectory() -> URL? {
+        do {
+            let newURL: URL = URL.documents.appendingPathComponent("Saved_Operations")
+            if !FileManager.default.fileExists(atPath: newURL.path) {
+                try FileManager.default.createDirectory(at: newURL, withIntermediateDirectories: false)
+            }
+            return newURL
+        } catch {
+            print("An error occurred getting/making the saved operations directory")
+        }
+        return nil
+    }
 }
--- Cowabunga/Views/UniqueToolsViews/AdvancedView.swift
+++ Cowabunga/Views/UniqueToolsViews/AdvancedView.swift
@@ -8,9 +8,104 @@
 import SwiftUI
 
 struct AdvancedView: View {
+    @State private var operations: [AdvancedObject] = [
+        CorruptingObject.init(operationName: "Test Operation", filePath: "/System/Library/PrivateFrameworks/SpringBoardHome.framework/podBackgroundViewLight.visualstyleset", singleApply: false, applyInBackground: false)
+    ]
+    
+    // lazyvgrid
+    private var gridItemLayout = [GridItem(.adaptive(minimum: 200))]
+    
     var body: some View {
         VStack {
-            
+            ScrollView {
+                LazyVGrid(columns: gridItemLayout) {
+                    ForEach(operations) { operation in
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.gray.opacity(0.4))
+                                .cornerRadius(10)
+                            
+                            VStack {
+                                // title
+                                HStack {
+                                    Text(operation.operationName)
+                                        .padding(5)
+                                        .font(.system(size: 22, weight: .heavy))
+                                        .lineLimit(1)
+                                    
+                                    Spacer()
+                                    
+                                    // edit button
+                                    Button(action: {
+                                        
+                                    }) {
+                                        Image(systemName: "square.and.pencil")
+                                            .font(.system(size: 25))
+                                    }
+                                    .padding(5)
+                                }
+                                
+                                // type of operation
+                                HStack {
+                                    Text("Type:")
+                                        .font(.system(size: 14, weight: .heavy))
+                                        .padding(.leading, 10)
+                                        .padding(.bottom, 5)
+                                    Spacer()
+                                    if operation is CorruptingObject {
+                                        Text("Corrupting")
+                                            .padding(.bottom, 5)
+                                            .padding(.trailing, 10)
+                                            .font(.system(size: 14, weight: .bold))
+                                    }
+                                }
+                                
+                                // uses background services
+                                HStack {
+                                    Text("Apply in Background:")
+                                        .font(.system(size: 14, weight: .heavy))
+                                        .padding(.leading, 10)
+                                        .padding(.bottom, 5)
+                                    
+                                    Spacer()
+                                    
+                                    if operation.applyInBackground == true {
+                                        Text("Yes")
+                                            .font(.system(size: 14, weight: .bold))
+                                            .padding(.trailing, 10)
+                                            .padding(.bottom, 5)
+                                            .foregroundColor(.green)
+                                    } else {
+                                        Text("No")
+                                            .font(.system(size: 14, weight: .bold))
+                                            .padding(.trailing, 10)
+                                            .padding(.bottom, 5)
+                                            .foregroundColor(.red)
+                                    }
+                                }
+                                
+                                // path
+                                HStack {
+                                    Text("Path:")
+                                        .font(.system(size: 14, weight: .heavy))
+                                        .padding(.leading, 10)
+                                        .padding(.bottom, 5)
+                                    
+                                    Spacer()
+                                    
+                                    Text(operation.filePath)
+                                        .font(.system(size: 14))
+                                        .padding(.trailing, 10)
+                                        .padding(.bottom, 5)
+                                }
+                            }
+                            .padding(10)
+                        }
+                        .padding(12)
+                    }
+                }
+            }
+            .navigationTitle("Custom Operations")
         }
     }
 }
