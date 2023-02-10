d0c111f14d7a5dd9c35ab036273687b270cfceb9
leminlimez <jirizarry317@gmail.com>
2023-02-10 13:05:53 -0500

    started plist

--- Cowabunga/Views/OperationViews/EditingOperationView.swift
+++ Cowabunga/Views/OperationViews/EditingOperationView.swift
@@ -12,16 +12,22 @@ struct EditingOperationView: View {
     var editing: Bool
     @State var operation: AdvancedObject
     
+    // basic properties
     @State var operationName: String = ""
     @State var filePath: String = ""
     @State var applyInBackground: Bool = false
     
+    // replacing properties
     @State var savedFilePath: String = "/"
     
     @State var replacingType: ReplacingObjectType = ReplacingObjectType.Imported
     @State var replacingPath: String = ""
     @State var replacingData: Data? = nil
     @State var backupData: Data? = nil
+    
+    // plist properties
+    @State var plistType: PropertyListSerialization.PropertyListFormat = .xml
+    @State var replacingKeys: [String: Any] = [:]
     
     @State var isImporting: Bool = false
     
@@ -51,14 +57,22 @@ struct EditingOperationView: View {
                             let corruptingAction = UIAlertAction(title: NSLocalizedString("Corrupting", comment: "operation type that corrupts/disables the file"), style: .default) { (action) in
                                 // change the type
                                 if !(operation is CorruptingObject) {
-                                    operation = CorruptingObject(operationName: operationName, filePath: filePath, singleApply: false, applyInBackground: applyInBackground)
+                                    operation = CorruptingObject(operationName: operationName, filePath: filePath, applyInBackground: applyInBackground)
                                 }
                             }
                             
                             let replacingAction = UIAlertAction(title: NSLocalizedString("Replacing", comment: "operation type that replaces a file"), style: .default) { (action) in
                                 // change the type
                                 if !(operation is ReplacingObject) {
-                                    operation = ReplacingObject(operationName: operationName, filePath: filePath, singleApply: false, applyInBackground: applyInBackground, overwriteData: Data("#".utf8), replacingType: ReplacingObjectType.Imported, replacingPath: "/Unknown")
+                                    operation = ReplacingObject(operationName: operationName, filePath: filePath, applyInBackground: applyInBackground, overwriteData: Data("#".utf8), replacingType: ReplacingObjectType.Imported, replacingPath: "/Unknown")
+                                }
+                            }
+                            
+                            let plistAction = UIAlertAction(title: NSLocalizedString("Plist", comment: "operation type that changes plist keys"), style: .default) { (action) in
+                                // change the type
+                                if !(operation is PlistObject) {
+                                    operation = PlistObject(operationName: operationName, filePath: filePath, applyInBackground: applyInBackground, plistType: plistType, replacingKeys: replacingKeys)
+                                    //operation = CorruptingObject(operationName: operationName, filePath: filePath, applyInBackground: applyInBackground)
                                 }
                             }
                             
@@ -133,6 +147,7 @@ struct EditingOperationView: View {
                     Text("Action")
                 }
                 
+                // MARK: Replacing Object Properties
                 if operation is ReplacingObject {
                     Section {
                         // MARK: Replacement Type
@@ -247,20 +262,61 @@ struct EditingOperationView: View {
                     }
                 }
                 
+                // MARK: Plist Object Operation
+                if operation is PlistObject {
+                    Section {
+                        // MARK: Plist Type
+                        HStack {
+                            Text("Plist Type:")
+                                .bold()
+                            Spacer()
+                            Button(action: {
+                                // create and configure alert controller
+                                let alert = UIAlertController(title: NSLocalizedString("Choose a plist type", comment: ""), message: "", preferredStyle: .actionSheet)
+                                
+                                // create the actions
+                                let xmlAction = UIAlertAction(title: "xml", style: .default) { (action) in
+                                    plistType = .xml
+                                }
+                                let binaryAction = UIAlertAction(title: "binary", style: .default) { (action) in
+                                    plistType = .binary
+                                }
+                                
+                                let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel) { (action) in
+                                    // cancels the action
+                                }
+                                
+                                // add the actions
+                                alert.addAction(xmlAction)
+                                alert.addAction(binaryAction)
+                                alert.addAction(cancelAction)
+                                
+                                let view: UIView = UIApplication.shared.windows.first!.rootViewController!.view
+                                // present popover for iPads
+                                alert.popoverPresentationController?.sourceView = view // prevents crashing on iPads
+                                alert.popoverPresentationController?.sourceRect = CGRect(x: view.bounds.midX, y: view.bounds.maxY, width: 0, height: 0) // show up at center bottom on iPads
+                                
+                                // present the alert
+                                UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true)
+                            }) {
+                                Text(replacingType.rawValue)
+                                    .multilineTextAlignment(.trailing)
+                            }
+                            .foregroundColor(.blue)
+                        }
+                    } header: {
+                        Text("Plist Data")
+                    }
+                }
+                
+                // MARK: Operation Actions
                 Section {
                     VStack {
                         // MARK: Save
                         Button(action: {
                             // apply the changes
                             UIApplication.shared.alert(title: NSLocalizedString("Saving operation...", comment: "apply button on custom operations"), body: NSLocalizedString("Please wait", comment: ""), animated: false, withButton: false)
-                            // set the properties of the operation
-                            operation.operationName = operationName
-                            operation.filePath = filePath
-                            operation.applyInBackground = applyInBackground
-                            if operation is ReplacingObject, let operation = operation as? ReplacingObject {
-                                operation.replacingType = replacingType
-                                operation.replacingPath = replacingPath
-                            }
+                            applyOperationProperties()
                             do {
                                 try AdvancedManager.saveOperation(operation: operation, category: category, replacingFileData: replacingData)
                                 UIApplication.shared.dismissAlert(animated: true)
@@ -283,6 +339,7 @@ struct EditingOperationView: View {
                             Button("Apply Once") {
                                 // apply the operation
                                 UIApplication.shared.alert(title: NSLocalizedString("Applying operation...", comment: "apply button on custom operations"), body: NSLocalizedString("Please wait", comment: ""), animated: false, withButton: false)
+                                applyOperationProperties()
                                 do {
                                     try operation.parseData()
                                     try operation.applyData()
@@ -331,6 +388,17 @@ struct EditingOperationView: View {
                     UIApplication.shared.alert(body: NSLocalizedString("An error occurred", comment: "") + ": \(error.localizedDescription)")
                 }
             }
+        }
+    }
+    
+    func applyOperationProperties() {
+        // set the properties of the operation
+        operation.operationName = operationName
+        operation.filePath = filePath
+        operation.applyInBackground = applyInBackground
+        if operation is ReplacingObject, let operation = operation as? ReplacingObject {
+            operation.replacingType = replacingType
+            operation.replacingPath = replacingPath
         }
     }
 }
---
+++ Cowabunga/Views/OperationViews/PlistEditView.swift
@@ -0,0 +1,17 @@
+//
+//  OperationPlistEditView.swift
+//  Cowabunga
+//
+//  Created by lemin on 2/10/23.
+//
+
+import SwiftUI
+
+struct PlistEditView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .navigationTitle(NSLocalizedString("Edit Plist Values", comment: "editing operation plist keys and values"))
+    }
+}
