d5e4c3ea3b75b1a18d256c741c6cd80915733917
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-02-09 19:35:58 -0500

    create directory of unnamed operations

--- Cowabunga/Controllers/AdvancedTools/AdvancedManager.swift
+++ Cowabunga/Controllers/AdvancedTools/AdvancedManager.swift
@@ -14,10 +14,22 @@ class AdvancedManager {
             if !FileManager.default.fileExists(atPath: newURL.path) {
                 try FileManager.default.createDirectory(at: newURL, withIntermediateDirectories: false)
             }
+            createUnnamedFolder(folderURL: newURL)
             return newURL
         } catch {
             print("An error occurred getting/making the saved operations directory")
         }
         return nil
+    }
+    
+    func createUnnamedFolder(folderURL: URL) {
+        do {
+            let unnamedURL = folderURL.appendingPathComponent("Unnamed")
+            if !FileManager.default.fileExists(atPath: unnamedURL.path) {
+                try FileManager.default.createDirectory(at: unnamedURL, withIntermediateDirectories: false)
+            }
+        } catch {
+            print("An error occurred making unnamed directory")
+        }
     }
 }
