88d3bfd42c858386a309fdb2a0b175e61158ac6e
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-12 13:20:37 -0500

    add it (broken rn)

---
+++ Cowabunga/Views/WhitelistViews/WhitelistFunctions.swift
@@ -0,0 +1,27 @@
+//
+//  WhitelistFunctions.swift
+//  Cowabunga
+//
+//  Created by Hariz Shirazi on 2023-02-12.
+//
+
+import Foundation
+import MacDirtyCowSwift
+
+let blankplist = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPCFET0NUWVBFIHBsaXN0IFBVQkxJQyAiLS8vQXBwbGUvL0RURCBQTElTVCAxLjAvL0VOIiAiaHR0cDovL3d3dy5hcHBsZS5jb20vRFREcy9Qcm9wZXJ0eUxpc3QtMS4wLmR0ZCI+CjxwbGlzdCB2ZXJzaW9uPSIxLjAiPgo8ZGljdC8+CjwvcGxpc3Q+Cg=="
+
+func overwriteBlacklist() -> Bool {
+    return MDC.overwriteFile(at: "/private/var/db/MobileIdentityData/Rejections.plist", with: try! Data(base64Encoded: blankplist)!)
+}
+
+func overwriteBannedApps() -> Bool {
+    return MDC.overwriteFile(at: "/private/var/db/MobileIdentityData/AuthListBannedUpps.plist", with: try! Data(base64Encoded: blankplist)!)
+}
+
+func overwriteCdHashes() -> Bool {
+    return MDC.overwriteFile(at: "/private/var/db/MobileIdentityData/AuthListBannedCdHashes.plist", with: try! Data(base64Encoded: blankplist)!)
+}
+
+func readFile(path: String) -> String? {
+    return (try? String?(String(contentsOfFile: path)) ?? "ERROR: Could not read from file! Are you running in the simulator or not unsandboxed?")
+}
---
+++ Cowabunga/Views/WhitelistViews/WhitelistView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  Whitelist
+//
+//  Created by Hariz Shirazi on 2023-02-03.
+//
+
+import SwiftUI
+
+struct WhitelistView: View {
+    @State var blacklist = true
+    @State var banned = true
+    @State var cdHash = true
+    @State var inProgress = false
+    @State var message = ""
+    @State var banned_success = false
+    @State var blacklist_success = false
+    @State var hash_success = false
+    @State var success = false
+    @State var success_message = ""
+
+    var body: some View {
+            List {
+                Section {
+                    Toggle("Overwrite Blacklist", isOn: $blacklist)
+                        .disabled(true)
+                        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                        .disabled(inProgress)
+                    Toggle("Overwrite Banned Apps", isOn: $banned)
+                        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                        .disabled(inProgress)
+                    Toggle("Overwrite CDHashes", isOn: $cdHash)
+                        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                        .disabled(inProgress)
+                } header: {
+                    Label("Options", systemImage: "gear")
+                }
+                Section {
+                    Button(
+                        action: {
+                            Haptic.shared.play(.heavy)
+                            inProgress = true
+                            
+                            if banned {
+                                banned_success = overwriteBannedApps()
+                            }
+                            if cdHash {
+                                hash_success = overwriteCdHashes()
+                            }
+                            success = overwriteBlacklist()
+                            
+                            // FIXME: Bad.
+                            if banned_success && hash_success {
+                                success_message = "Successfully removed: Blacklist, Banned Apps, CDHashes\nDidn't overwrite: none"
+                            } else if !banned_success && hash_success {
+                                success_message = "Successfully removed: Blacklist, CDHashes\nDidn't overwrite: Banned Apps"
+                            } else if banned_success && !hash_success {
+                                success_message = "Successfully removed: Blacklist, Banned Apps\nDidn't overwrite: CDHashes"
+                            } else {
+                                success_message = "Successfully removed: Blacklist\nDidn't overwrite: Banned Apps, CDHashes"
+                            }
+                            
+                            if success {
+                                UIApplication.shared.alert(title: "Success", body: success_message, withButton: true)
+                                inProgress = false
+                                Haptic.shared.notify(.success)
+                            } else {
+                                UIApplication.shared.alert(title: "Error", body: "An error occurred while writing to the file.", withButton: true)
+                                inProgress = false
+                                Haptic.shared.notify(.error)
+                            }
+                            inProgress = false
+                        },
+                        label: { Label("Apply", systemImage: "app.badge.checkmark") }
+                    )
+                } header: {
+                    Label("Make It So, Number One", systemImage: "arrow.right.circle")
+                }
+                
+                Section {
+                    NavigationLink(destination: FileContentsView()) {
+                        Text("View contents of blacklist files")
+                    }
+                } header : {
+                    Label("Advanced", systemImage: "wrench.and.screwdriver")
+                }
+            }
+        
+        .navigationTitle("Whitelist")
+    }
+}
+
+struct WhitelistView_Previews: PreviewProvider {
+    static var previews: some View {
+        WhitelistView()
+    }
+}
