ffb49cc17acdaa12782b2ad0eba79b75e3875a6c
leminlimez <jirizarry317@gmail.com>
2023-02-13 13:03:47 -0500

    remove unneeded font stuff

--- Cowabunga/Controllers/FontChanger/stripttc.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-int stripttc_handlefile(const char *filename);
--- Cowabunga/Controllers/FontChanger/ttcpad.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-@class NSData;
-NSData *repack_ttc(NSData *original, bool delete_noncritical, bool allow_corrupt_loca);
