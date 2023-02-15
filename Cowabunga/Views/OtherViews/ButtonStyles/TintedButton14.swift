fe2c6426fdbaad7d0109a570120a747015041c43
Matthew Anisovich <anisovich.matthew@gmail.com>
2023-02-15 17:40:40 +0000

    Add info option to TintedButton

--- Cowabunga/Views/OtherViews/ButtonStyles/TintedButton14.swift
+++ Cowabunga/Views/OtherViews/ButtonStyles/TintedButton14.swift
@@ -9,14 +9,33 @@ import SwiftUI
 
 struct FullwidthTintedButton: ButtonStyle {
     var color: Color
+    var info: String?
     
     func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .padding(15)
-            .frame(maxWidth: .infinity)
-            .background(color.opacity(0.2))
-            .cornerRadius(8)
-            .foregroundColor(color)
+        ZStack {
+            configuration.label
+                .padding(15)
+                .frame(maxWidth: .infinity)
+                .background(color.opacity(0.2))
+                .cornerRadius(8)
+                .foregroundColor(color)
+            
+            if let info = info {
+                HStack {
+                    Spacer()
+                    Button {
+                        UIApplication.shared.alert(title: "Info", body: info)
+                    } label: {
+                        Image(systemName: "info")
+                            .frame(width: 32, height: 32)
+                            .background(MaterialView(.systemChromeMaterial))
+                            .cornerRadius(10)
+                            .foregroundColor(color)
+                    }
+                }
+                .padding(.horizontal, 10)
+            }
+        }
     }
 }
 
@@ -31,3 +50,17 @@ struct TintedButton: ButtonStyle {
             .foregroundColor(color)
     }
 }
+
+
+#if DEBUG
+@available(iOS 15, *)
+struct FullwidthTintedButton_Previews: PreviewProvider {
+    static var previews: some View {
+        Button("Exaple") {
+            
+        }
+        .buttonStyle( FullwidthTintedButton(color: .red, info: "Test info"))
+        .padding()
+    }
+}
+#endif
