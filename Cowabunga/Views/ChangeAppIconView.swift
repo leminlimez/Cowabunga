f9eefb38a5c337ad3c91e18cffa0eec514c95ee9
leminlimez <jirizarry317@gmail.com>
2023-02-16 20:20:24 -0500

    choosing alternate icons

--- Cowabunga/Views/ChangeAppIconView.swift
+++ Cowabunga/Views/ChangeAppIconView.swift
@@ -23,10 +23,13 @@ struct ChangeAppIconView: View {
                                 .cornerRadius(12)
                             Text(appIcon.description)
                             Spacer()
-                            CheckboxView(isSelected: viewModel.selectedAppIcon == appIcon)
+                            Image(systemName: "checkmark")
+                                .opacity(viewModel.selectedAppIcon == appIcon ? 1 : 0)
+                                .padding(10)
+                                .font(.system(size: 20))
                         }
                         .padding(EdgeInsets(top: 14, leading: 16, bottom: 14, trailing: 16))
-                        .background(Color.gray)
+                        .background(Color(uiColor14: .secondarySystemBackground))
                         .cornerRadius(20)
                         .onTapGesture {
                             withAnimation {
@@ -38,24 +41,12 @@ struct ChangeAppIconView: View {
                     .padding(.vertical, 40)
             }
         }
+        .navigationTitle("Choose app Icon")
     }
 }
 
 struct ChangeAppIconView_Previews: PreviewProvider {
     static var previews: some View {
         ChangeAppIconView()
-    }
-}
-
-struct CheckboxView: View {
-    let isSelected: Bool
-
-    private var image: UIImage {
-        let imageName = isSelected ? "icon-checked" : "icon-unchecked"
-        return UIImage(imageLiteralResourceName: imageName)
-    }
-
-    var body: some View {
-        Image(uiImage: image)
     }
 }
