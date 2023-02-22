afc650175b9ca70558e91ffe947707ae775a9d4e
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-02-22 18:33:20 -0500

    enable color ._.

---
+++ Cowabunga/Views/UniqueToolsViews/CCModifierView.swift
@@ -0,0 +1,22 @@
+//
+//  CCModifierView.swift
+//  Cowabunga
+//
+//  Created by lemin on 2/22/23.
+//
+
+import SwiftUI
+
+struct CCModifierView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct CCModifierView_Previews: PreviewProvider {
+    static var previews: some View {
+        CCModifierView()
+    }
+}
